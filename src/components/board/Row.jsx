1d26e19474cd053380578f2e447e68693b386bb2
Uliana <ulianav95v@gmail.com>
2023-01-09 18:58:49 +0300

    fixed small thing

--- src/components/board/Row.jsx
+++ src/components/board/Row.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import styled from "styled-components";
-import { useSelector } from "react-redux";
-import FlippingLetterBox from "./FlippingLetterBox";
+import React from 'react';
+import styled from 'styled-components';
+import { useSelector } from 'react-redux';
+import FlippingLetterBox from './FlippingLetterBox';
 
 const Wrapper = styled.div`
   display: flex;
@@ -11,18 +11,9 @@ const Wrapper = styled.div`
 function Row({ guess, colors, wordChecked }) {
   const { lettersNumber, newGame } = useSelector((state) => state.word);
 
-  const obj = {
-    5: [0, 1, 2, 3, 4],
-    6: [0, 1, 2, 3, 4, 5],
-    7: [0, 1, 2, 3, 4, 5, 6],
-    8: [0, 1, 2, 3, 4, 5, 6, 7],
-    9: [0, 1, 2, 3, 4, 5, 6, 7, 8],
-    10: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-  };
-
   return (
     <Wrapper>
-      {obj[lettersNumber].map((letter, i) => (
+      {[...Array(lettersNumber)].map((letter, i) => (
         <FlippingLetterBox
           key={letter}
           color={colors && colors[i]}
